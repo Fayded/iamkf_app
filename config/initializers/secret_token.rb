b513314b40af19db9d10d289d30dc562b602683d
Fayded <kevin.d.fay@gmail.com>
2013-05-20 12:44:42 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IamkfApp::Application.config.secret_token = '9ba8ba0446a02d54426f6e97a80e6f3eb800ea592d0fd1c72323ac6e204ed897e96c15ef77d646612a3fa0feb2a16bbdce1bfe323e0a30f49d59684e99faaf0e'
